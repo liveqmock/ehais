7fb67d822598c2ba30f62c9790471228e2e88e47
lgj628 <lgj628@126.com>
2017-09-27 17:52:09 +0800

    dd

--- e-shop/src/main/webapp/js/ehais/member_order.js
+++ e-shop/src/main/webapp/js/ehais/member_order.js
@@ -1,15 +1,17 @@
 var orderData = {};
 var pageData = {all:1,paywaiting:1,payed:1,shiped:1,returned:1};
 var jroll;
+var status;
+var loaded;
 $(function(){
-	
-	orderinfo_list($("dl dd:first").attr("for"));
+	status = $("dl dd:first").attr("for");
+	orderinfo_list();
 	
 	$("dl dd").click(function(){
-		
+		status = $(this).attr("for");
 		if($(this).attr("for") != location.hash){
 			$("dl dd").removeClass("active");
-			$(this).addClass("active");
+			$(this).addClass("active");			
 			if(orderData[$(this).attr("for")] == null){
 				orderinfo_list($(this).attr("for"));
 			}else{
@@ -22,49 +24,36 @@ $(function(){
 	
 	
 
-	console.log(parseFloat($(window).height())+"=="+parseFloat($(".wm").css('padding-top').replaceAll("px","")));
 	$("#jorder").height(parseFloat($(window).height()) - parseFloat($(".wm").css('padding-top').replaceAll("px","")));
 	
-	
+	loaded = "";
 	jroll = new JRoll("#jorder", {scrollBarY:false});
-	jroll.pulldown({
-	    refresh: function(complete) {
-	        //完成加载数据的操作后回调执行complete方法
-	        console.log("complete();");
-	        complete();
-	    }
+	
+	jroll.on("touchEnd", function() {
+		
+		if (this.y >= 44) {
+			console.log("下拉刷新中");
+			if(loaded=="")loaded="pull";
+		}else if (this.y < this.maxScrollY - 10){
+			console.log("上拉刷新中");
+			if(loaded=="")loaded="up";
+		}
 	});
 	
-	jroll.infinite({
-//		loadingTip : "<div></div>",
-	    getData: function(page, callback, errorCallback) {
-	    	console.log("page();"+page);
-	    	jroll.options.total = 10;
-	    	callback([{
-	            "index": "ffffff",
-	            "text": "自定义图标+开启blank"
-	          }]);
-//	        $.ajax({
-//	            url : "getdata.php",
-//	            data : condition,
-//	            type : "POST",
-//	            success : function(data) {
-//	                jroll.options.total = data.total;
-//	                callback(data.items);
-//	            },
-//	            error: function() {
-//	                errorCallback()
-//	            }
-//	        });
-	    },
-	    template: "<div class='item'>{{=_obj.index}}、{{=_obj.text}}</div>" //每条数据模板
+	jroll.on("scrollEnd", function() {
+		console.log(loaded);
+		if(loaded == "pull"){
+			pageData[status] = 1;
+			orderinfo_list();
+		}else if(loaded == "up"){
+			orderinfo_list();
+		}
 	});
-	
-	
 	
 });
 
-function orderinfo_list(status){
+function orderinfo_list(){
+	loaded = "ing";
 	var order_status = null;
 	var pay_status = null;
 	var shipping_status = null;
@@ -72,18 +61,39 @@ function orderinfo_list(status){
 	if(status == "rw"){order_status = 1 ; pay_status = 1;}
 	if(status == "rd"){order_status = 4 ; pay_status = 4;}
 	$.ajax({
-		url : "/ws/orderinfo_list",
+		url : "/ws/orderinfo_list",type:"post",dataType:"json",
 		data:{
 			order_status:order_status,
 			pay_status:pay_status,
 			shipping_status:shipping_status,
 			page:pageData[status],
-			rows:10
+			rows:3
 			},
 		success : function(result){
+			loaded = "";
+			
 			if(orderData[status] == null){
 				orderData[status] = result;
 				driverOrderInfo(result);
+			}else{
+				var oresult = orderData[status];
+				
+				console.log(result.rows.length +"---"+ orderData[status].rows.length);
+				
+				for(var k in result.rows ){
+					console.log("result.rows[k].orderSn:"+result.rows[k].orderSn);
+					orderData[status].rows.push(result.rows[k]);
+				}
+				
+				
+				if(result.map != null && result.map.order_goods_list != null && result.map.order_goods_list.length > 0){
+					for(var k in result.map.order_goods_list ){
+						orderData[status].map.order_goods_list.push(result.map.order_goods_list[k]);
+					}
+				}
+				
+				driverOrderInfo(orderData[status]);
+				
 			}
 		}
 	});
