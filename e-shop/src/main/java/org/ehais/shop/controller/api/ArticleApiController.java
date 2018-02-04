da1803188baa552af66dbe0016814b538b070a20
lgj628 <lgj628@126.com>
2018-02-04 16:00:01 +0800

    asdfsdf

--- e-shop/src/main/java/org/ehais/shop/controller/api/ArticleApiController.java
+++ e-shop/src/main/java/org/ehais/shop/controller/api/ArticleApiController.java
@@ -17,6 +17,8 @@ import org.ehais.epublic.model.EHaiArticleCat;
 import org.ehais.epublic.model.EHaiArticleCatExample;
 import org.ehais.epublic.model.EHaiArticleExample;
 import org.ehais.shop.controller.api.include.ArticleIController;
+import org.ehais.shop.service.ArticleCatService;
+import org.ehais.shop.service.ArticleService;
 import org.ehais.tools.ReturnObject;
 import org.ehais.util.Bean2Utils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +40,10 @@ public class ArticleApiController extends ArticleIController{
 	@Autowired
 	private EHaiArticleMapper haiArticleMapper;
 	
+	@Autowired
+	private ArticleCatService articleCatService;
+	@Autowired
+	private ArticleService articleService;
 	
 	@ResponseBody
 	@RequestMapping("/article_save")
@@ -50,83 +56,89 @@ public class ArticleApiController extends ArticleIController{
 		try {
 //			Bean2Utils.printEntity(article);
 			
-			Integer parent_cat_id = 0 ;
-			if(StringUtils.isNotBlank(parent_cat_name)){
-				EHaiArticleCatExample ace = new EHaiArticleCatExample();
-				ace.createCriteria()
-				.andStoreIdEqualTo(article.getStoreId())
-				.andCatNameEqualTo(parent_cat_name)
-				.andParentIdEqualTo(0)
-				.andModuleEqualTo(EArticleModuleEnum.ARTICLE);
-				List<EHaiArticleCat> articleCatList = haiArticleCatMapper.selectByExample(ace);
-				if(articleCatList != null && articleCatList.size() > 0){
-					parent_cat_id = articleCatList.get(0).getCatId();
-				}else{
-					EHaiArticleCat ac = new EHaiArticleCat();
-					ac.setCatName(parent_cat_name);
-					ac.setParentId(0);
-					ac.setStoreId(article.getStoreId());
-					ac.setModule(EArticleModuleEnum.ARTICLE);
-					ac.setIsValid(true);ac.setCatType(1);ac.setKeywords("");ac.setCatDesc("");ac.setSortOrder(1);ac.setShowInNav(true);ac.setParentId(0);
-					haiArticleCatMapper.insert(ac);
-					parent_cat_id = ac.getCatId();
-				}
-				
-			}
+//			Integer parent_cat_id = 0 ;
+//			if(StringUtils.isNotBlank(parent_cat_name)){
+//				EHaiArticleCatExample ace = new EHaiArticleCatExample();
+//				ace.createCriteria()
+//				.andStoreIdEqualTo(article.getStoreId())
+//				.andCatNameEqualTo(parent_cat_name)
+//				.andParentIdEqualTo(0)
+//				.andModuleEqualTo(EArticleModuleEnum.ARTICLE);
+//				List<EHaiArticleCat> articleCatList = haiArticleCatMapper.selectByExample(ace);
+//				if(articleCatList != null && articleCatList.size() > 0){
+//					parent_cat_id = articleCatList.get(0).getCatId();
+//				}else{
+//					EHaiArticleCat ac = new EHaiArticleCat();
+//					ac.setCatName(parent_cat_name);
+//					ac.setParentId(0);
+//					ac.setStoreId(article.getStoreId());
+//					ac.setModule(EArticleModuleEnum.ARTICLE);
+//					ac.setIsValid(true);ac.setCatType(1);ac.setKeywords("");ac.setCatDesc("");ac.setSortOrder(1);ac.setShowInNav(true);ac.setParentId(0);
+//					haiArticleCatMapper.insert(ac);
+//					parent_cat_id = ac.getCatId();
+//				}
+//				
+//			}
+//			
+//			System.out.println(cat_name+"==========="+parent_cat_name+"-----------"+parent_cat_id);
+//			
+//			//获取分类
+//			EHaiArticleCatExample acExp = new EHaiArticleCatExample();
+//			acExp.createCriteria()
+//			.andStoreIdEqualTo(article.getStoreId())
+//			.andCatNameEqualTo(cat_name)
+//			.andParentIdEqualTo(parent_cat_id)
+//			.andModuleEqualTo(EArticleModuleEnum.ARTICLE);
+//			List<EHaiArticleCat> articleCatList = haiArticleCatMapper.selectByExample(acExp);
+//			EHaiArticleCat ac = null;
+//			if(articleCatList == null || articleCatList.size() == 0){System.out.println(cat_name+"**********"+parent_cat_id);
+//				ac = new EHaiArticleCat();
+//				ac.setCatName(cat_name);
+//				ac.setParentId(parent_cat_id);
+//				ac.setStoreId(article.getStoreId());
+//				ac.setModule(EArticleModuleEnum.ARTICLE);
+//				ac.setIsValid(true);ac.setCatType(1);ac.setKeywords("");ac.setCatDesc("");ac.setSortOrder(1);ac.setShowInNav(true);
+//				haiArticleCatMapper.insert(ac);
+//			}else{System.out.println(cat_name+"&&&&&&&&&&&&&&&&&&&&"+parent_cat_id);
+//				ac = articleCatList.get(0);
+//			}
+			EHaiArticleCat cate = new EHaiArticleCat();
+			cate.setCatName(cat_name);
+			articleCatService.articleCatSave(cate, parent_cat_name, article.getStoreId());
 			
-			System.out.println(cat_name+"==========="+parent_cat_name+"-----------"+parent_cat_id);
-			
-			//获取分类
-			EHaiArticleCatExample acExp = new EHaiArticleCatExample();
-			acExp.createCriteria()
-			.andStoreIdEqualTo(article.getStoreId())
-			.andCatNameEqualTo(cat_name)
-			.andParentIdEqualTo(parent_cat_id)
-			.andModuleEqualTo(EArticleModuleEnum.ARTICLE);
-			List<EHaiArticleCat> articleCatList = haiArticleCatMapper.selectByExample(acExp);
-			EHaiArticleCat ac = null;
-			if(articleCatList == null || articleCatList.size() == 0){System.out.println(cat_name+"**********"+parent_cat_id);
-				ac = new EHaiArticleCat();
-				ac.setCatName(cat_name);
-				ac.setParentId(parent_cat_id);
-				ac.setStoreId(article.getStoreId());
-				ac.setModule(EArticleModuleEnum.ARTICLE);
-				ac.setIsValid(true);ac.setCatType(1);ac.setKeywords("");ac.setCatDesc("");ac.setSortOrder(1);ac.setShowInNav(true);
-				haiArticleCatMapper.insert(ac);
-			}else{System.out.println(cat_name+"&&&&&&&&&&&&&&&&&&&&"+parent_cat_id);
-				ac = articleCatList.get(0);
-			}
-			
-			article.setCatId(ac.getCatId());
 			
 			
-			EHaiArticleExample aExp = new EHaiArticleExample();
-			aExp.createCriteria()
-			.andStoreIdEqualTo(article.getStoreId())
-			.andModuleEqualTo(EArticleModuleEnum.ARTICLE)
-			.andTitleEqualTo(article.getTitle())
-			.andLinkEqualTo(article.getLink()) ;
+			articleService.articleSave(cate, article);
 			
-			List<EHaiArticle> articleList = haiArticleMapper.selectByExample(aExp);
-			if(articleList == null || articleList.size() == 0){
-				Date date = new Date();
-				article.setModule(EArticleModuleEnum.ARTICLE);
-				if(article.getArticleDate() == null)article.setArticleDate(date);
-				article.setCreateDate(date);
-				article.setUpdateDate(date);
-				article.setIsOpen(true);
-				article.setOpenType(true);
-				if(StringUtils.isNotBlank(article.getAuthor()))article.setAuthor("");
-				if(StringUtils.isNotBlank(article.getAuthorEmail()))article.setAuthorEmail("");
-				if(StringUtils.isNotBlank(article.getKeywords()))article.setKeywords("");
-				if(StringUtils.isNotBlank(article.getFileUrl()))article.setFileUrl("");
-								
-//				haiArticleMapper.insert(article);
-				haiArticleMapper.insertSelective(article);
-			}else{
-				article.setArticleId(articleList.get(0).getArticleId());
-				haiArticleMapper.updateByPrimaryKeySelective(article);
-			}
+//			
+//			
+//			EHaiArticleExample aExp = new EHaiArticleExample();
+//			aExp.createCriteria()
+//			.andStoreIdEqualTo(article.getStoreId())
+//			.andModuleEqualTo(EArticleModuleEnum.ARTICLE)
+//			.andTitleEqualTo(article.getTitle())
+//			.andLinkEqualTo(article.getLink()) ;
+//			
+//			List<EHaiArticle> articleList = haiArticleMapper.selectByExample(aExp);
+//			if(articleList == null || articleList.size() == 0){
+//				Date date = new Date();
+//				article.setModule(EArticleModuleEnum.ARTICLE);
+//				if(article.getArticleDate() == null)article.setArticleDate(date);
+//				article.setCreateDate(date);
+//				article.setUpdateDate(date);
+//				article.setIsOpen(true);
+//				article.setOpenType(true);
+//				if(StringUtils.isNotBlank(article.getAuthor()))article.setAuthor("");
+//				if(StringUtils.isNotBlank(article.getAuthorEmail()))article.setAuthorEmail("");
+//				if(StringUtils.isNotBlank(article.getKeywords()))article.setKeywords("");
+//				if(StringUtils.isNotBlank(article.getFileUrl()))article.setFileUrl("");
+//								
+////				haiArticleMapper.insert(article);
+//				haiArticleMapper.insertSelective(article);
+//			}else{
+//				article.setArticleId(articleList.get(0).getArticleId());
+//				haiArticleMapper.updateByPrimaryKeySelective(article);
+//			}
 			ReturnObject<EHaiArticle> rm = new ReturnObject<EHaiArticle>();
 			rm.setModel(article);
 			
