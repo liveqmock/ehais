172e45e73382aa2b0bb560351736a2a2c05677b5
root <root@localhost>
2018-03-14 00:40:01 +0800

    six 2

--- e-Junit/src/main/java/org/ehais/Junit/reptile/Six_6hewawaReptile.java
+++ e-Junit/src/main/java/org/ehais/Junit/reptile/Six_6hewawaReptile.java
@@ -1,5 +1,34 @@
 package org.ehais.Junit.reptile;
 
-public class Six_6hewawaReptile {
+import org.ehais.util.EHtmlUnit;
+import org.ehais.util.EHttpClientUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+public class Six_6hewawaReptile extends ArticleCommonReptile{
+	
+	String url = "https://www.6hewawa.com/2018.htm";
+	
+	@Test
+	public void list() {
+		
+		try {
+			String result = EHtmlUnit.getAjaxPage(url);
+//			System.out.println(content);
+			Document doc = Jsoup.parse(result,"utf-8");
+			String content = doc.getElementsByClass("content").text();
+//			System.out.println(content);
+			String list[] = content.split("\n");
+			
+			for (String string : list) {
+				System.out.println(string.substring(0,3)+"====="+string.substring(9));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
