8113d4654c9b6a893a426ff8fcc1dd5f644f453a
lgj628 <lgj628@126.com>
2018-07-09 10:26:30 +0800

    更新orderclassify

--- ehais-common/src/main/java/org/ehais/enums/EOrderClassifyEnum.java
+++ ehais-common/src/main/java/org/ehais/enums/EOrderClassifyEnum.java
@@ -1,6 +1,7 @@
 package org.ehais.enums;
 
 public class EOrderClassifyEnum {
+	
 	public static String shop = "shop";
 	public static String dining = "dining";
 	public static String sale = "sale";//销售
@@ -9,5 +10,32 @@ public class EOrderClassifyEnum {
 	public static String purchase_back = "purchase_back";//采购退货
 	public static String proceeds = "proceeds";//收款
 	public static String payment = "payment";//付款
+	
+	/**
+	 * 获取昵称
+	 * @param str
+	 * @return
+	 */
+	public static String getOrderClassify(String str){
+		String ret = "";
+		if(str.equals(shop)){
+			ret = shop;
+		}else if(str.equals(dining)){
+			ret = dining;
+		}else if(str.equals(sale)){
+			ret = "销售";
+		}else if(str.equals(sale_back)){
+			ret = "销售退货";
+		}else if(str.equals(purchase)){
+			ret = "采购";
+		}else if(str.equals(purchase_back)){
+			ret = "采购退货";
+		}else if(str.equals(proceeds)){
+			ret = "收款";
+		}else if(str.equals(payment)){
+			ret = "付款";
+		}
+		return ret;
+	}
 	
 }
